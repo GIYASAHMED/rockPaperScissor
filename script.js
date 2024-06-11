94273d254875dc63ad4d7bdf5e952ffb97b93940
giyaswebdev <giyas.ahmed257@gmail.com>
2024-06-11 16:43:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let playerEl = document.getElementById('player')
+let computerEl = document.getElementById('computer')
+let scoreEl = document.getElementById('score')
+let rockEl = document.getElementById('rock')
+let paperEl = document.getElementById('paper')
+let scissorEl = document.getElementById('scissor')
+
+
+
